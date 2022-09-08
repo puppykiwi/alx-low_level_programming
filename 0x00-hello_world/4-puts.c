ad72b894ae7773609143bbe5b3504712c4434e26
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-09-08 20:48:24 +0300

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 
-int main(void)
-    /*
-    *this function prints a string!
-    */
-{
+int main(void) //this function prints a string!
+{ 
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
