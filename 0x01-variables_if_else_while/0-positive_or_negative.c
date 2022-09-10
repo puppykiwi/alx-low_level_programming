496d9a505444a3e1ee42fe359efddd1235bfd30e
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-09-10 12:58:02 +0300

    betty spaces

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
@@ -9,11 +10,14 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-    {printf("n is positive\n");}
-    else if (n == 0)
-    {printf("n is equal 0\n");}
-    else 
-    {printf("n is negative\n");}
-	return (0);
+if (n > 0)
+{
+printf("n is positive\n"); }
+else if (n == 0)
+{
+printf("n is equal 0\n"); }
+else 
+{
+printf("n is negative\n"); }
+return (0);
 }
