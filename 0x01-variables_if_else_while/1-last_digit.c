2d3cc2dededc8527fa69b25231ff23cb27e23303
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-09-10 13:26:46 +0300

    shows j

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,12 +15,12 @@
 j = n % 10;
 if (j > 5)
 {
-printf("Last digit of %d is and is greater than 5\n", n); }
+printf("Last digit of %d is %d and is greater than 5\n", n, j); }
 else if (j == 0)
 {
-printf("Last digit of %d is and is 0\n", n); }
+printf("Last digit of %d is %d and is 0\n", n, j); }
 else if (j < 6 && j != 0)
 {
-printf("Last digit of %d is and is less than 6 and not 0\n", n); }
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, j); }
 return (0);
 }
