c7a53350c9119402e9455ae85771b5322fca7c60
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-09-10 16:06:15 +0300

    task12

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/*
+*betty technicalities
+*/int main(void)
+{
+int i, j, k;
+i = 48;
+j = 48;
+while (i < 58)
+{
+j = i + 1;
+while (j < 58)
+{
+k = j + 1;
+while (k < 58)
+{
+putchar(i);
+putchar(j);
+putchar(k);
+if (i != 55 || j != 57 || k != 58)
+{
+putchar(44);
+putchar(32);
+}
+k++;
+}
+j++;
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
