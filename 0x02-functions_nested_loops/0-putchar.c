628ec79f77d46e38c0e9040fbba5a4e8fa5c0d80
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-09-12 09:41:22 +0300

    used while loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,6 +3,14 @@
 *betty technicalities
 */int main(void)
 {
-printf("_putchar\n");
+char string[8]="_putchar";
+int i;
+i = 0;
+while (i!= 8)
+{
+putchar(string[i]);
+i++;
+}
+putchar('\n');
 return (0);
 }
