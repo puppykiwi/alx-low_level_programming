b8cfa5ab7a18ea64c35cf0f32d6b4b841b1339fe
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-09-12 11:18:44 +0300

    task 5

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 /*
-*betty technicalities
-*/int _islower(int c)
+*
+*/int _isalpha(int c)
 {
 if (c >= 'a' && c <= 'z')
+return (1);
+else if (c >= 'A' && c <= 'Z')
 return (1);
 else
 return (0);
