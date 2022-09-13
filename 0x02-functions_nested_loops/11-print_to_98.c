4ee577622f97d2d9621905188a6e168fa6550d6b
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-09-13 11:34:41 +0300

    I used printf and IDGAF

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+/*
+*betty technicalities
+*/void print_to_98(int n)
+{
+if (n < 98)
+{
+while (n <= 98)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+n++;
+}
+}
+else if (n > 98)
+{
+while (n >= 98)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+n--;
+}
+}
+else
+{
+printf("98");
+}
+putchar('\n');
+}
