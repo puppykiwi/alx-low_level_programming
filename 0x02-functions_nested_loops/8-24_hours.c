80fa3bdd686446aad3b957986c94136015cd9ef6
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-09-13 10:22:17 +0300

    task9

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/*
+*betty technicalities
+*/void jack_bauer(void)
+{
+int h, m;
+for (h = 0; h < 24; h++)
+{
+for (m = 0; m < 60; m++)
+{
+putchar((h / 10) + '0');
+putchar((h % 10) + '0');
+putchar(':');
+putchar((m / 10) + '0');
+putchar((m % 10) + '0');
+putchar('\n');
+}
+}
+}
