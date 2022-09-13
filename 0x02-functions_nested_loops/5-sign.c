027d66d4a9f7024677586981dd348fcb9d48fcc5
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-09-13 09:07:53 +0300

    task6

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/*
+*betty technicalities
+*/int print_sign(int n)
+{
+if (n > 0)
+{
+putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+putchar('0');
+return (0);
+}
+else if (n < 0)
+{
+putchar('-');
+return (-1);
+}
+putchar('\n');
+}
