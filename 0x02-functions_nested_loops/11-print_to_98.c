346372c817d1060d31911205a3520fe8a098e707
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-09-15 13:09:17 +0300

    Ive given up

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,32 +10,12 @@ while (n <= 98)
 {
 if (n < 10)
 {
-if (n > -1)
-{
-_putchar(n+'0');
-}
-else if (n < 0)
-{
-if ( n < -99)
-{
-_putchar((n / 100)+ '0');
-_putchar(((n % 100)/ 10)+ '0');
-_putchar(((n % 100) % 10)+ '0');
-}
-else if (n > -100)
-{
-int q,w;
-q = n / 100;
-w = n % 10;
-printf("%d", q);
-printf("%d", w);
-}
-}
+_putchar(n + '0');
 }
 else if (n > 9)
 {
-_putchar((n / 10)+ '0' );
-_putchar((n % 10)+ '0' );
+_putchar((n / 10) + '0');
+_putchar((n % 10) + '0');
 }
 if (n != 98)
 {
@@ -45,21 +25,20 @@ _putchar(' ');
 n++;
 }
 }
-
 else if (n > 98)
 {
 while (n >= 98)
 {
-if ( n > 99)
+if (n > 99)
 {
-_putchar((n / 100)+ '0');
-_putchar(((n % 100)/ 10)+ '0');
-_putchar(((n % 100) % 10)+ '0');
+_putchar((n / 100) + '0');
+_putchar(((n % 100) / 10) + '0');
+_putchar(((n % 100) % 10) + '0');
 }
 else if (n < 100)
 {
-_putchar((n / 10)+ '0' );
-_putchar((n % 10)+ '0' );
+_putchar((n / 10) + '0');
+_putchar((n % 10) + '0');
 }
 if (n != 98)
 {
