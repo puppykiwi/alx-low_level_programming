186d41b649eafa9e24fba7759cb86ef44b9a4f3d
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-09-15 21:41:38 +0300

    pleaseeeee

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,48 +1,65 @@
 #include "main.h"
+#include<stdio.h>
 /*
 *betty technicalities
 */void times_table(void)
 {
 int a, b;
-int d;
+int d, e, w;
 for (a = 0; a < 10; a++)
 {
-_putchar('0');
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-for (b = 1; b < 10; b++)
-{
-d = a * b;
-if (d < 10)
-{
-_putchar(d + '0');
-if (b != 9)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-}
-else
-{
+    putchar('0');
+    putchar(',');
+    putchar(' ');
+    putchar(' ');
+        for (b = 1; b < 10; b++)
+        {
+        d = a * b;
+        e = b + 1;
+        w = a * e;
+            if (d < 10)
+            {
+            putchar(d + '0');
+                if (b != 9)
+                {
+                    if (w < 10)
+                    {
+                    putchar(',');
+                    putchar(' ');
+                    putchar(' ');
+                    }
+                    else
+                    {
+                    putchar(',');
+                    putchar(' ');
+                    }
+                }
+                else 
+                {
+                    
+                }
+            }
+            else
+            {
+            putchar((d / 10) + '0');
+            putchar((d % 10) + '0');
+                if (b != 9)
+                {
+                putchar(',');
+                putchar(' ');
+                }
+                else
+                {
 
-}
-}
-else
-{
-_putchar((d / 10) + '0');
-_putchar((d % 10) + '0');
-if (b != 9)
-{
-_putchar(',');
-_putchar(' ');
-}
-else
-{
+                }
+                }
+                }
+                putchar('\n');
+                }
+                }
 
-}
-}
-}
-_putchar('\n');
-}
+int main(void)
+{
+    times_table();
+    return (0);
 }
