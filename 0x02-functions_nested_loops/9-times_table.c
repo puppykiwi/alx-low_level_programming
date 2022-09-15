c7be6e3b0b82434898753668206da71d03c0e66e
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-09-15 11:45:43 +0300

    please

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,16 +17,30 @@ d = a * b;
 if (d < 10)
 {
 _putchar(d + '0');
+if (b != 9)
+{
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 }
 else
 {
+_putchar(' ');
+}
+}
+else
+{
 _putchar((d / 10) + '0');
 _putchar((d % 10) + '0');
+if (b != 9)
+{
 _putchar(',');
 _putchar(' ');
+}
+else
+{
+_putchar(' ');
+}
 }
 }
 _putchar('\n');
