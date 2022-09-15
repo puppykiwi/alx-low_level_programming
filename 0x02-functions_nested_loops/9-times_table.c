68c704c4cab8d37eced7fa3abba2c092c741ede6
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-09-15 11:22:39 +0300

    finally unde

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,20 +7,21 @@ int a, b;
 int d;
 for (a = 0; a < 10; a++)
 {
+_putchar('0');
 for (b = 1; b < 10; b++)
 {
-_putchar(',');
-_putchar(' ');
 d = a * b;
 if (d < 10)
 {
-_putchar(' ');
+_putchar(d);
 }
 else
 {
 _putchar((d / 10) + '0');
 _putchar((d % 10) + '0');
 }
+_putchar(',');
+_putchar(' ');
 }
 _putchar('\n');
 }
