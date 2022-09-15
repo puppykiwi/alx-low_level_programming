416fb193587f35ee2292b010ea5fc63a8b2d565e
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-09-15 12:22:49 +0300

    double digit nos

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -29,10 +29,17 @@ else if (n > 98)
 {
 while (n >= 98)
 {
+if ( n > 99)
+{
 _putchar((n / 100)+ '0');
 _putchar(((n % 100)/ 10)+ '0');
 _putchar(((n % 100) % 10)+ '0');
-
+}
+else if (n < 100)
+{
+_putchar((n / 10)+ '0' );
+_putchar((n % 10)+ '0' );
+}
 if (n != 98)
 {
 _putchar(',');
