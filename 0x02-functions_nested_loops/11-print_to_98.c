afba5f2265268769decadba597757742b3cf52c2
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-09-15 12:41:04 +0300

    this better work

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,15 +4,34 @@
 */void print_to_98(int n)
 {
 
-if (n < '0')
+if (n < 0)
 {
- n = n * -1;
+ while (n < 98)
+ {
+    if (n < 0)
+    {
+_putchar((n / 100)+ '0');
+_putchar(((n % 100)/ 10)+ '0');
+_putchar(((n % 100) % 10)+ '0');  
+    }
+    else if (n >= 0)
+    {
+        if (n < 10)
+        {
+            _putchar(n+'0');
+        }
+        else if (n > 9)
+        {
+            _putchar((n / 10)+ '0' );
+            _putchar((n % 10)+ '0' );
+        }
+    }
+    n++;
+ }
 }
-else if (n > '0')
+
+else if (n > 0)
 {
-
-}
-
 if (n < 98)
 {
 while (n <= 98)
@@ -26,7 +45,6 @@ else if (n > 9)
 _putchar((n / 10)+ '0' );
 _putchar((n % 10)+ '0' );
 }
-
 if (n != 98)
 {
 _putchar(',');
@@ -35,6 +53,7 @@ _putchar(' ');
 n++;
 }
 }
+
 else if (n > 98)
 {
 while (n >= 98)
@@ -64,4 +83,5 @@ _putchar('9');
 _putchar('8');
 }
 _putchar('\n');
+}
 }
