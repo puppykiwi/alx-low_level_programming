f224bac851244d49adf5e8ceaba80ab51dcecee8
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-09-16 09:57:17 +0300

    task4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*
+*betty technicalities
+*/void print_most_numbers(void)
+{
+char num[]="01345679";
+int i;
+for (i = '0'; i != '9'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
