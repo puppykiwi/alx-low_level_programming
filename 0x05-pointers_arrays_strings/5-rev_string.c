af45b5a4500c875189219c518b25a5fc1b54d349
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-09-19 18:32:27 +0300

    task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include<string.h>
+/*
+*betty technicalities
+*/void rev_string(char *s)
+{
+int i;
+i = strlen(s);
+while (i > -1)
+{
+_putchar(s[i]);
+i--;
+}
+_putchar('\n');
+}
