7a3c6d5b00025b3950ded320b7bc0d1cceec9901
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-09-19 19:38:26 +0300

    task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,11 +4,21 @@
 *betty technicalities
 */void rev_string(char *s)
 {
-int i, tmp, len = _strlen(s);
-int i, tmp, len = _strlen(s);
+int i, max, half;
+char first, last;
+i = 0;
+while (s[i] != '\0')
 {
-tmp = *(s + i);
-*(s + i) = *(s + len - i - 1);
-*(s + len - i - 1) = tmp;
+i++;
+}
+max = i - 1;
+half = max / 2;
+while (half >= 0)
+{
+first = s[max - half];
+last = s[half];
+s[half] = first;
+s[max - half] = last;
+half--;
 }
 }
