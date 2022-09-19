d8a5fab9a7f2a863ef6396e3e3f7401e21d56358
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-09-19 19:01:54 +0300

    added odd

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,23 +5,18 @@
 */void puts_half(char *str)
 {
 int j;
-if ((strlen(str) / 2) == 0)
+if ((strlen(str) % 2) == 0)
 {
 j = (strlen(str) / 2);
-while (str[j] != '\0')
-{
-_putchar(str[j]);
-j++;
 }
-}
-else
+else if ((strlen(str) % 2) != 0)
 {
 j = ((strlen(str) - 1) / 2);
+}
 while (str[j] != '\0')
 {
 _putchar(str[j]);
 j++;
-} 
 }
 _putchar('\n');
 }
