a9f390020b416df5459c21668681ce01672b1eac
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-09-19 18:45:44 +0300

    task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,14 +4,13 @@
 *betty technicalities
 */void puts2(char *str)
 {
-    int i;
-    i = 0;
-    while (str[i] != '\0')
-    {
-        if (i%2 == 0)
-        {
-            _putchar(i);
-        }
-        i++;
-    }
+int i;
+i = 0;
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
 }
