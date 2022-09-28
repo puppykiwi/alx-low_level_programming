dae8c5cf16f9dbb7817955f8501f1aeccf39b7a0
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-09-28 14:19:11 +0300

    betty

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,15 +7,14 @@
   *@b: byte to fill mem with
   *@n: number of bytes pointed to by s
   *Return: s
-  */
-char *_memset(char *s, char b, unsigned int n)
+  */char *_memset(char *s, char b, unsigned int n)
 {
 
-	unsigned int i;
+unsigned int i;
 
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-	return (s);
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
 }
