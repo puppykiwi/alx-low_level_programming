7e949de9b3d6e4f10d4d6b8a6e643f91cdc057b6
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-09-28 14:59:11 +0300

    test

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,22 +3,18 @@
 *betty technicalities
 */char *_strchr(char *s, char c)
 {
-char *b;
-int i;
-i = 0;
+	int i;
 
-while (s[i] != '\0')
-{
-if ((s[i] == c))
-{
-b = &s[i];
-return (b);
-}
-else
-{
-i++;
-}
-}
+	i = 0;
 
-return (NULL);
+	while (*(s + i) != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (&(s[i]));
+		}
+		else
+			i++;
+	}
+	return (NULL);
 }
