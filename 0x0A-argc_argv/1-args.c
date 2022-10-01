2aae01fe9401bae70e635d32f69f75a0b33ad1f3
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-10-01 12:56:21 +0300

    effing betty

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /*
 *betty technicalities
-*/int main(int argc,char *argv[]__attribute__((unused)))
+*/int main(int argc, char *argv[]__attribute__((unused)))
 {
-    printf("%d\n", argc - 1);
-    return (0);
+printf("%d\n", argc - 1);
+return (0);
 }
