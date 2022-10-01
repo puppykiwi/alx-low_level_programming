f93d8e338ce5d12d3151d5b81a68a95393d1399f
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-10-01 14:17:13 +0300

    fixed number checker

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+
+int is_number(char *s);
 /*
 *betty technicalities
 */int main(int argc, char *argv[])
 {
 int result = 0;
 int j = 1;
-
+int check;
 if (argc == 1)
 {
 printf("0\n");
@@ -17,7 +19,8 @@ else
 {
 while (j != argc)
 {
-if (isdigit(atoi(argv[j])))
+check = is_number(argv[j]);
+if (check == 1)
 {
 result = result + atoi(argv[j]);
 j++;
@@ -31,4 +34,24 @@ j++;
 }
 printf("%d\n", result);
 }
+}
+
+/*
+*betty technicalities
+*/int is_number(char *s)
+{
+int i;
+i = 0;
+while (*(s + i) != '\0')
+{
+if (*(s + i) >= '0' && *(s + i) <= '9')
+{
+i++;
+}
+else
+{
+return (0);
+}
+}
+return (1);
 }
