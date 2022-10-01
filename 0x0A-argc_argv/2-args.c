4a172e6eaf195b6452ad34b1172d1bf01dfaa8a5
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-10-01 12:59:31 +0300

    task2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+*betty technicalities
+*/int main (int argc, char *argv[])
+{
+    int count = 0;
+    while (count != argc)
+    {
+        printf("%s\n",argv[count]);
+        ++count;
+    }
+    return (0);
+}
