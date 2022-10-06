e4d83bd21adeec18541ec4a0821bc86f2805e009
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-10-06 13:50:29 +0300

    test

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -5,6 +5,12 @@
 *betty technicalities
 */char *str_concat(char *s1, char *s2)
 {
+
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+
 char *result;
 int totalen = (strlen(s1) + strlen(s2));
 int len1, len2;
@@ -19,11 +25,6 @@ while (*(s2 + len2) != '\0')
 {
 len2++;
 }
-
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
 
 result = malloc(totalen * sizeof(char) + 1);
 
