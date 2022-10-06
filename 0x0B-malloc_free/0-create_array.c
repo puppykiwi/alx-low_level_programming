19e68a5360534b69483ed55ea520377a7f45f56d
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-10-06 11:49:46 +0300

    betty

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,24 +4,24 @@
 *betty technicalities
 */char *create_array(unsigned int size, char c)
 {
-    char* test;
-    int i , j = size;
-    test = malloc(size * sizeof(char));
-    if (test == NULL)
-    {
-        return (NULL);
-    }
+char *test;
+int i, j = size;
+test = malloc(size * sizeof(char));
+if (test == NULL)
+{
+return (NULL);
+}
 
-    if (size > 0 )
-    {
-        for (i = 0; i < j; i++)
-        {
-            test[i] = c;
-        }
-        return (test);
-    }
-    else
-    {
-        return (NULL);
-    }
+if (size > 0)
+{
+for (i = 0; i < j; i++)
+{
+test[i] = c;
+}
+return (test);
+}
+else
+{
+return (NULL);
+}
 }
