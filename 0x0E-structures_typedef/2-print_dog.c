86366dd75915d9841bace247e228ea06d17a03a5
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-10-17 16:24:51 +0300

    task2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,33 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/*
+*betty technicalities
+*/void print_dog(struct dog *d)
+{
+    if (d == NULL)
+    {}
+    else
+    {
+        if ((*d).name == NULL)
+        {
+            printf("Name: (nil)\n");
+        }
+        else if ((*d).name != NULL)
+        {
+            printf("Name: %s\n", (*d).name);
+        }
+
+        printf("Age: %lf\n", (*d).age);
+
+        if ((*d).owner == NULL)
+        {
+            printf("Owner: (nil)\n");
+        }
+        else if ((*d).owner != NULL)
+        {
+            printf("Owner: %s\n", (*d).owner);
+        }
+
+    }
+}
