8a7898c63d0ed773f35978473b42417133ec19f2
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-10-17 16:33:27 +0300

    task3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,10 +10,10 @@ int _putchar(char c);
 	char *name;
 	float age;
 	char *owner;
-} dog;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog *new_dog(char *name, float age, char *owner);
-void free_dog(dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif /* !DOG_H */
