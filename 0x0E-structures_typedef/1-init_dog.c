f66b1be06e767b5824de7312380e7f6b7747f4c7
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-10-17 16:14:08 +0300

    task1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,11 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/*
+*betty technicalities
+*/void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
