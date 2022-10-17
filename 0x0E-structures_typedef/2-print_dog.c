c1baa59a29647764aab0ee371344ab517ff2ad8e
johnraymwendwa <johnraymwendwa04@gmail.com>
2022-10-17 16:31:42 +0300

    task2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,33 +1,32 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /*
 *betty technicalities
 */void print_dog(struct dog *d)
 {
-    if (d == NULL)
-    {}
-    else
-    {
-        if ((*d).name == NULL)
-        {
-            printf("Name: (nil)\n");
-        }
-        else if ((*d).name != NULL)
-        {
-            printf("Name: %s\n", (*d).name);
-        }
+if (d != NULL)
+{
+if ((*d).name == NULL)
+{
+printf("Name: (nil)\n");
+}
+else if ((*d).name != NULL)
+{
+printf("Name: %s\n", (*d).name);
+}
 
 printf("Age: %f\n", (*d).age);
 
-        if ((*d).owner == NULL)
-        {
-            printf("Owner: (nil)\n");
-        }
-        else if ((*d).owner != NULL)
-        {
-            printf("Owner: %s\n", (*d).owner);
-        }
+if ((*d).owner == NULL)
+{
+printf("Owner: (nil)\n");
+}
+else if ((*d).owner != NULL)
+{
+printf("Owner: %s\n", (*d).owner);
+}
 
-    }
+}
 }
