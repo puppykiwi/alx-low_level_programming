af34ff1527648e8abc0bd3ebfeb9cef095be3c7b
puppykiwi <johnraymwendwa04@gmail.com>
2023-07-11 09:09:25 +0300

    task 0 tired

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,16 +14,15 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-size_t i;
-
-if (!array || size <= 0)
+size_t i = 0;
+if (!array || size == 0)
 return (-1);
-
-for (i = 0; i < size; i++)
+while (i < size)
 {
-printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-if (array[i] == value) /* if (*(array + i) == value) */
+printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+if (array[i] == value)
 return (i);
+i++;
 }
 return (-1);
 }
