3b98575418ac03ff68bb038cd878f7a77b8eef8d
puppykiwi <johnraymwendwa04@gmail.com>
2023-07-11 08:41:19 +0300

    header bump

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,6 +2,9 @@
 #define SEARCH_ALGOS_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
 /* PROTOTYPES */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
