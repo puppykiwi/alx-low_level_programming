558f09e026661de817b3b483e9c567b907ed1d0f
puppykiwi <johnraymwendwa04@gmail.com>
2023-07-13 11:55:46 +0300

    update

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,4 +1,7 @@
 #include "search_algos.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
   * jump_search - Searches for a value in a sorted array
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <math.h>
 
 /* PROTOTYPES */
 int linear_search(int *array, size_t size, int value);
