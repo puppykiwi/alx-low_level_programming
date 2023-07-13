a427426cc7bf8113e320ce0c30d4189d78861ae7
puppykiwi <johnraymwendwa04@gmail.com>
2023-07-13 12:09:30 +0300

    header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -14,5 +14,7 @@ int binary_search_index(int *array, size_t l, size_t r, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+int advanced_binary_recursive(int *array, size_t l, size_t r, int value);
 
 #endif /* SEARCH_ALGOS_H */
