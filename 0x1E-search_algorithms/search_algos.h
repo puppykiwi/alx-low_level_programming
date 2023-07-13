4a9d5cc9b41bc4e9187387f9cb24fe8fa4c94f6b
puppykiwi <johnraymwendwa04@gmail.com>
2023-07-13 12:18:03 +0300

    header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -18,4 +18,21 @@ int advanced_binary(int *array, size_t size, int value);
 int advanced_binary_recursive(int *array, size_t l, size_t r, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 
+/* STRUCTURES */
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
 #endif /* SEARCH_ALGOS_H */
