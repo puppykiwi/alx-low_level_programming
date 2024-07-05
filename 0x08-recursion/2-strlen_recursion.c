1ea6192d720553edf57d02126a45575063cc1c57
Johnray Mwendwa <johnraymwendwa04@gmail.com>
2024-07-05 12:01:45 +0300

    task2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,10 +1,18 @@
 #include "main.h"
-#include <stdio.h>
-/*
- *betty technicalities
- */int _strlen_recursion(char *s)
+/**
+ * _strlen_recursion - length of string
+ * @s:string
+ * Return:int
+ */
+int _strlen_recursion(char *s)
 {
-if (*s != '\0')
-return (_strlen_recursion(s + 1) + 1);
+	int i = 0;
+
+	if (*s)
+	{
+		i++;
+		i += _strlen_recursion(s + 1);
+
+	}
+	return (i);
 }
-//what is keeping track of the number of calls
